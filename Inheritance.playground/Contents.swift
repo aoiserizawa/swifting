dea1c77819b367bb5bbc4bc4ac9caf7906ec5751
Luisito Yumang <alvinvaldez@Luisitos-Mac-mini.local>
2016-02-24 02:06:26 +0800

    added inheritance with overriding method sample'

--- Inheritance.playground/Contents.swift
+++ Inheritance.playground/Contents.swift
@@ -5,5 +5,54 @@ import UIKit
 class BodyPart{
     var name:String = "BodyParts"
     
+    func printName(){
+        print(name)
+    }
+    
+    func returnInteger(parameter:Int) -> Int{
+        return 5 * parameter
+    }
+    
+    var type:String{
+        return "generic body part"
+    }
+    
+    func printType(){
+        print("My \(name) is a \(type)")
+    }
+}
+
+var bodyPart = BodyPart()
+
+bodyPart.printName()
+
+class Hand: BodyPart {
+    var numberOfFingers = 5
+    
+    // Overriding a method
+    override func printName() {
+        super.printName()
+        print("subclass")
+    }
+    // Overriding a method
+    override func returnInteger(parameter:Int) -> Int {
+        var newValue = super.returnInteger(parameter)
+        return newValue * parameter
+    }
+    
+    override var type:String{
+        get {
+            return "Limb"
+        }
+    }
     
 }
+
+var hand = Hand()
+hand.name = "Hand"
+hand.printName()
+
+
+print(hand.returnInteger(5))
+hand.printType()
+
